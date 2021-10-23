15dc18012dc775ded380fe9c01ef6b86e503387b
masegy <augyerislioga.s@gmail.com>
2021-10-23 11:38:22 +0700

    first commit

---
+++ src/parts/IconText.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="/beranda" type="link">
+      BRI<span>Queue.</span>
+    </Button>
+  );
+}
