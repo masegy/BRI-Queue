f50f565f380d92bbd16fa0678750719ebe13bb52
Augyeris Lioga Seandrio <83108316+masegy@users.noreply.github.com>
2021-12-16 19:58:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import AntrianBankPage from "pages/AntrianBankPage";
 import DaftarBank from "pages/DaftarBank";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://tranquil-island-89451.herokuapp.com/test/";
+axios.defaults.baseURL = "http://tranquil-island-89451.herokuapp.com";
 const PrivateRoute = (props) => {
   const token = localStorage.getItem("token");
   const history = useHistory();
